c1fee9ccc3fa83e5461869f6c3eaa32953006272
PravinkumarRD <dabade.pravinkumar@gmail.com>
2024-01-11 17:55:25 +0530

    Add files via upload

---
+++ BajajInterviewApp/BajajInterviewApp/Candidate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BajajInterviewApp
+{
+    public delegate void InterviewDelegate();
+    internal class Candidate
+    {
+        public event InterviewDelegate SelectedCandidate;
+        public event InterviewDelegate RejectedCandidate;
+        private int m_CandidateId;
+        //GETTER/SETTER Property
+        public int CandidateId
+        {
+            get
+            {
+                return m_CandidateId;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Candidate Id must be greater than Zero!");
+                }
+                m_CandidateId = value;
+            }
+        }
+        //Auto Implemented Properties
+        public string ContactName { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public int TotalMarks { get; set; }
+
+        //public string CalculateResult(int totalMarks,out bool confirmation)
+        public string CalculateResult(int totalMarks, ref bool confirmation)
+        {
+            if (totalMarks > 80)
+            {
+                if(SelectedCandidate != null)
+                {
+                    SelectedCandidate();
+                }
+               confirmation = true;
+                return $"{ContactName} has been selected in Bajaj! Pune!";
+            }
+            else
+            {
+                if (RejectedCandidate != null)
+                {
+                    RejectedCandidate();
+                }
+                //confirmation = false;
+                return $"{ContactName} has been Rejected! Try after 6 months!";
+            }
+        }
+        public void PrintCities(params string[] cities)
+        {
+            for (int i = 0; i < cities.Length; i++)
+            {
+                Console.WriteLine($"City - {cities[i]}");
+            }
+        }
+    }
+}
---
+++ BajajInterviewApp/BajajInterviewApp/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace BajajInterviewApp
+{
+    internal class Program
+    {
+        private static void Db2StudentRegistry()
+        {
+            Console.WriteLine("Student Record Saved in DB2 Server!");
+        }
+        private static void OracleStudentRegistry()
+        {
+            Console.WriteLine("Student Record Saved in Oracle Database!");
+        }
+        private static void MySqlStudentRegistry()
+        {
+            Console.WriteLine("Student Record Saved in MySQL Server Database!");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Bajaj Interview Application!");
+            try
+            {
+                Candidate candidate = new Candidate();
+                candidate.SelectedCandidate += OracleStudentRegistry;
+                candidate.SelectedCandidate += Db2StudentRegistry;
+                candidate.RejectedCandidate += MySqlStudentRegistry;
+                candidate.CandidateId = 9929;
+                candidate.ContactName = "Alisha C.";
+                candidate.City = "Mumbai";
+                candidate.TotalMarks = 98;
+                if (candidate.TotalMarks<95)
+                {
+                    candidate.SelectedCandidate -= Db2StudentRegistry;
+                }
+                bool bajajConfirmation = false;
+                string result = candidate.CalculateResult(candidate.TotalMarks, ref bajajConfirmation);
+                Console.WriteLine($"Bajaj Confirmation value is {bajajConfirmation}");
+                Console.WriteLine(result);
+                Console.WriteLine("");
+                candidate.PrintCities("A", "B");
+                Console.WriteLine("");
+                candidate.PrintCities("A", "B", "C");
+                Console.WriteLine("");
+                candidate.PrintCities("A", "B", "C", "D", "E");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+}
